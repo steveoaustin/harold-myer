786567069aff66e08754a05114a95f04e8a93801
steveoaustin <steveoaustin812@gmail.com>
2020-10-25 22:18:26 -0400

    fix OG share

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,20 +1,22 @@
-import './App.scss';
+import "./App.scss";
 
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router";
 
-import Footer from './Components/Footer/Footer';
-import NavBar from './Components/NavBar/NavBar';
-import AboutUs from './Components/Pages/AboutUs/AboutUs';
-import Contact from './Components/Pages/Contact/Contact';
-import Home from './Components/Pages/Home/Home';
-import Payment from './Components/Pages/Payment/Payment';
-import AirConditioning from './Components/Pages/Services/AirConditioning/AirConditioning';
-import FuelOil from './Components/Pages/Services/FuelOil/FuelOil';
-import Installation from './Components/Pages/Services/Installation/Installation';
-import Services from './Components/Pages/Services/Services';
-import Testimonials from './Components/Pages/Testimonials/Testimonials';
-import ScrollToTop from './Components/Tools/ScrollToTop';
+import AboutUs from "./Components/Pages/AboutUs/AboutUs";
+import AirConditioning from "./Components/Pages/Services/AirConditioning/AirConditioning";
+import Contact from "./Components/Pages/Contact/Contact";
+import FB from "../Media/graphics/fbShare.png";
+import Footer from "./Components/Footer/Footer";
+import FuelOil from "./Components/Pages/Services/FuelOil/FuelOil";
+import Helmet from "react-helmet";
+import Home from "./Components/Pages/Home/Home";
+import Installation from "./Components/Pages/Services/Installation/Installation";
+import NavBar from "./Components/NavBar/NavBar";
+import Payment from "./Components/Pages/Payment/Payment";
+import ScrollToTop from "./Components/Tools/ScrollToTop";
+import Services from "./Components/Pages/Services/Services";
+import Testimonials from "./Components/Pages/Testimonials/Testimonials";
 
 require("es6-object-assign/auto");
 
@@ -22,6 +24,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <meta property="og:title" content="Harold Myers Inc." />
+          <meta property="og:image" content={FB} />
+        </Helmet>
         <NavBar />
         <ScrollToTop>
           <Switch>
