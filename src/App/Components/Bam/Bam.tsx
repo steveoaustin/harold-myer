e403ac2c735e3ff12f73296fb3bf2349578381b2
steveoaustin812 <steveoaustin812@gmail.com>
2023-10-02 09:38:35 -0400

    erase tyler math

--- src/App/Components/Bam/Bam.tsx
+++ src/App/Components/Bam/Bam.tsx
@@ -1,22 +1,19 @@
-import './Bam.scss';
+import "./Bam.scss";
 
-import React from 'react';
+import React from "react";
 
-import bam from '../../../Media/Images/Compressed/bam.png';
+import bam from "../../../Media/graphics/BAM Digital Frost Logo.png";
 
 class Bam extends React.Component {
   render() {
     return (
-      <div className="bam">
-        <a href="https://bamheroes.com" className="bam-container">
-          <div className="bam-text">Built By</div>
-          <img
-            src={bam}
-            alt={"bam websites logos marketing"}
-            className="bam-logo"
-          />
-        </a>
-      </div>
+      <a href="https://bamdigital.io" className="bam">
+        <img
+          src={bam}
+          alt={"BAM Digital Logo - I am the singularity"}
+          className="bam-logo"
+        />
+      </a>
     );
   }
 }
